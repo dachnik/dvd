1a5f6b0faf1517fe0d37f9d258169cc6a50c244c
Slava <v.v.ignatenko@gmail.com>
2016-04-01 18:51:55 +0300

    Ответ на задание№2

--- visa.py
+++ visa.py
@@ -1,1 +1,21 @@
-print("Hello! Саша пиши задание №2")
+while True:
+    str1 = input('Введи первое число')
+    str2 = input('Введи второе число')
+
+    a = float(str1)
+    b = float(str2)
+
+    operator = input('Введите операцию')
+
+    if operator == '+':  
+        s = a + b        
+    if operator == '-':
+        s = a - b
+    if operator == '*':
+        s = a* b 
+    if operator == '/':
+        s = a / b
+    print(s)
+    if operator == 'exit':
+        break
+print('Завершение')
