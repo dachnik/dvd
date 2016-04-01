2ba592f3eb7cb091c2aae68412e2ae748dd00966
Slava <v.v.ignatenko@gmail.com>
2016-04-01 18:02:09 +0300

    Вариант решения №3

--- task2.py
+++ task2.py
@@ -1,18 +1,21 @@
-str1 = input('Введи первое число')
-str2 = input('Введи второе число')
+while True:
+    str1 = input('Введи первое число')
+    str2 = input('Введи второе число')
 
-a = float(str1)
-b = float(str2)
+    a = float(str1)
+    b = float(str2)
 
-operator = input('Введите операцию')
-if operator == '+':  
-    s = a + b        
-if operator == '-':
-    s = a - b
-if operator == '*':
-    s = a* b 
-if operator == '/':
-    s = a / b
-print(s)
+    operator = input('Введите операцию')
 
-  
+    if operator == '+':  
+        s = a + b        
+    if operator == '-':
+        s = a - b
+    if operator == '*':
+        s = a* b 
+    if operator == '/':
+        s = a / b
+    print(s)
+    if operator == 'exit':
+        break
+print('Завершение')
